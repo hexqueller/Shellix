956ceb61be1a0f67e970483cf5415a0b936a888b
Дмитрий Беседин <dima.nesed@gmail.com>
2024-11-10 17:11:28 +0300

    Setup shell execute

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
-
+import subprocess
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -14,14 +13,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Execute shell command and return the result."""
+    command = update.message.text  # Получаем текст команды от пользователя
 
+    try:
+        # Выполняем команду и захватываем вывод
+        result = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        # В случае ошибки возвращаем её
+        result = f"Error:\n{e.output}"
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    # Отправляем результат в формате `bash`
+    await update.message.reply_text(f"```bash\n{result}\n```", parse_mode='MarkdownV2')
 
 
 def main() -> None:
@@ -30,16 +34,14 @@ def main() -> None:
         print("Token not found")
         exit(1)
 
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
+    # Start the bot
     application = Application.builder().token(bot_token).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    # on non-command message - execute as shell command
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, execute))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
