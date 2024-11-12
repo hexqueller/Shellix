da8d85e487610abdaf7f3a3c7e5c3c3745482750
Дмитрий Беседин <dima.nesed@gmail.com>
2024-11-12 11:59:48 +0300

    Add autotag for dockerhub

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Check if user's container exists; create it if not."""
     user_id = update.effective_user.id
     container_name = f"user_container_{user_id}"
 
@@ -29,7 +28,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("Контейнер уже существует.")
 
 async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Execute shell command in the user's container and return the result."""
     user_id = update.effective_user.id
     container_name = f"user_container_{user_id}"
     command = update.message.text  # Получаем текст команды от пользователя
@@ -53,28 +51,24 @@ async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             stderr=subprocess.STDOUT
         )
     except subprocess.CalledProcessError as e:
-        # В случае ошибки возвращаем её
         result = f"Error:\n{e.output}"
 
     # Отправляем результат в формате `bash`
     await update.message.reply_text(f"```bash\n{result}\n```", parse_mode='MarkdownV2')
 
 def main() -> None:
+    # Забираем из системы токен
     bot_token = os.getenv("TOKEN")
     if not bot_token:
         print("Token not found")
         exit(1)
 
-    # Start the bot
     application = Application.builder().token(bot_token).build()
 
-    # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
 
-    # on non-command message - execute as shell command
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, execute))
 
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
